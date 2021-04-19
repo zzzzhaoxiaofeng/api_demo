6dd9d1c7b8c236e698d40679c148ddc99656fb7f
zhaoxiaofeng1130@163.com <zhaoxiaofeng>
2021-04-19 16:27:08 +0800

    修改run

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import csv
 
 if __name__ == '__main__':
 
-    # tmp = time.strftime("%Y%m%d-%H%M%S",time.localtime(time.time()))
+    tmp = time.strftime("%Y%m%d-%H%M%S",time.localtime(time.time()))
     # pytest.main(['-s', f'--alluredir=./report/{tmp}/json/', '--reruns=3', '--reruns-delay=1'])
     # subprocess.call(f"allure generate ./report/{tmp}/json -o ./report/{tmp}/html",shell=True)
 
